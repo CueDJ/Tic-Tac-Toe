29848c37f6c3b149a5359c3c83c56d10269031ca
Jesper Heese <JesperHeese@outlook.com>
2023-09-13 19:40:41 +0200

    function for circle and added player switching

--- sketch.js
+++ sketch.js
@@ -11,15 +11,7 @@ function draw() {
   background(220);
   text(('mouse Y:' + mouseY), 20, 40) //remove later
   text(('mouse x:' + mouseX), 20, 20) //remove later
-  rect(125,125,250,250);
-  rect(125,375,250,250);
-  rect(125,625,250,250);
-  rect(375,125,250,250);
-  rect(375,375,250,250);
-  rect(375,625,250,250);
-  rect(625,125,250,250);
-  rect(625,375,250,250);
-  rect(625,625,250,250);
+  rect(125,125,250,250); rect(125,375,250,250); rect(125,625,250,250); rect(375,125,250,250); rect(375,375,250,250); rect(375,625,250,250); rect(625,125,250,250); rect(625,375,250,250); rect(625,625,250,250);
   cross(posX = 175, posY = 175) //box1 pos 
   cross(posX = 425, posY = 175) //box2 pos
   cross(posX = 675, posY = 175) //box3 pos 
@@ -28,7 +20,17 @@ function draw() {
   cross(posX = 675, posY = 425) //box6 pos
   cross(posX = 175, posY = 675) //box7 pos 
   cross(posX = 425, posY = 675) //box8 pos
-  cross(posX = 675, posY = 675) //box9 pos 
+  cross(posX = 675, posY = 675) //box9 pos
+  circle2(posX = 250, posY = 250) //box1 pos
+  circle2(posX = 500, posY = 250) //box2 pos
+  circle2(posX = 750, posY = 250) //box3 pos
+  circle2(posX = 250, posY = 500) //box4 pos
+  circle2(posX = 500, posY = 500) //box5 pos
+  circle2(posX = 750, posY = 500) //box6 pos
+  circle2(posX = 250, posY = 750) //box7 pos
+  circle2(posX = 500, posY = 750) //box8 pos
+  circle2(posX = 750, posY = 750) //box9 pos
+
 }
 function mousePressed() {
   
@@ -37,9 +39,11 @@ function mousePressed() {
     if(box1 == 0) {
       if(player == 1) {
         box1 = 1
+        player = 2
       }
       else if(player == 2) {
         box1 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -51,9 +55,11 @@ function mousePressed() {
     if(box2 == 0) {
       if(player == 1) {
         box2 = 1
+        player = 2
       }
       else if(player == 2) {
         box2 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -65,9 +71,11 @@ function mousePressed() {
     if(box3 == 0) {
       if(player == 1) {
         box3 = 1
+        player = 2
       }
       else if(player == 2) {
         box3 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -79,9 +87,11 @@ function mousePressed() {
     if(box4 == 0) {
       if(player == 1) {
         box4 = 1
+        player = 2
       }
       else if(player == 2) {
         box4 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -93,9 +103,11 @@ function mousePressed() {
     if(box5 == 0) {
       if(player == 1) {
         box5 = 1
+        player = 2
       }
       else if(player == 2) {
         box5 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -107,9 +119,11 @@ function mousePressed() {
     if(box6 == 0) {
       if(player == 1) {
         box6 = 1
+        player = 2
       }
       else if(player == 2) {
         box6 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -121,9 +135,11 @@ function mousePressed() {
     if(box7 == 0) {
       if(player == 1) {
         box7 = 1
+        player = 2
       }
       else if(player == 2) {
         box7 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -135,9 +151,11 @@ function mousePressed() {
     if(box8 == 0) {
       if(player == 1) {
         box8 = 1
+        player = 2
       }
       else if(player == 2) {
         box8 = 2
+        player = 1
       }
       else {
       console.log('code brok')
@@ -149,9 +167,12 @@ function mousePressed() {
     if(box9 == 0) {
       if(player == 1) {
         box9 = 1
+        player = 2
       }
       else if(player == 2) {
         box9 = 2
+        player = 1
+        
       }
       else {
       console.log('code brok')
@@ -163,4 +184,9 @@ function cross() {
   strokeWeight(5)  
   line(0 + posX,0 + posY,150 + posX,150 + posY)
   line(0 + posX,150 + posY,150 + posX,0 +posY)
+}
+function circle2() {
+  noFill()
+  circle(0 + posX, 0 +posY, 200)
+  fill('white')
 }
