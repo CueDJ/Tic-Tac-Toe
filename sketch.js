9043cef530d9004e2415879450f0c04135c16f4f
Jesper Heese <JesperHeese@outlook.com>
2023-09-14 14:44:04 +0200

    added comments and removed unnecessary things

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-let player = 1
-let box1 = 0, box2 = 0, box3 = 0, box4 = 0, box5 = 0, box6 = 0, box7 = 0, box8 = 0, box9 = 0
-let turns = 0
-function setup() {
-  createCanvas(1000, 1000);
-  frameRate(10)
-  strokeWeight(5)
-}
+let player = 1 //to keep track of which player it is and at the same time used for if someone wins or ties
+let box1 = 0, box2 = 0, box3 = 0, box4 = 0, box5 = 0, box6 = 0, box7 = 0, box8 = 0, box9 = 0 //9 vars for the boxes
+let turns = 0 //used to identify a tie 
+let player1wins = 0
+let player2wins = 0
+let gamecount = 1
+function setup() { createCanvas(1000, 1000); } //creates the canvas itself
 function draw() {
   background(220);
   textSize(50)
@@ -14,25 +13,28 @@ function draw() {
   text("Tic Tac Toe", 370, 75)
   textSize(20)
   if (player == 1) {
-    text("Player 1 (cross)", 430, 110)
+    text("Player 1 (cross)", 430, 110) //notes that its players 1 turn
   }
   if (player == 2) {
-    text("Player 2 (circle)", 430, 110)
+    text("Player 2 (circle)", 430, 110) //notes that its players 2 turn
   }
   if (player == 3) {
     text('Player 1 wins!', 430, 110)
-    text('Press R to reset!', 420,950)
+    text('Press R to reset and update stats!', 350, 910) //player 1 wins and enables reset
   }
   if (player == 4) {
     text('Player 2 wins!', 430, 110)
-    text('Press R to reset!', 420,950)
+    text('Press R to reset and update stats!', 350, 910) //player 2 wins and enables reset
   }
   if (player == 5) {
-    text('Tie!', 450, 110)
-    text('Press R to reset!', 420,950)
+    text('Tie!', 480, 110)
+    text('Press R to reset and update stats!', 350, 910)//no one wins and enables reset
   }
+  text('player 1 has won ' + player1wins + " times!", 120, 950)
+  text('player 2 has won ' + player2wins + " times!", 625, 950)
+  text("Game " + gamecount + "!", 450, 950)
   fill('white')
-  rect(125, 125, 250, 250); rect(125, 375, 250, 250); rect(125, 625, 250, 250); rect(375, 125, 250, 250); rect(375, 375, 250, 250); rect(375, 625, 250, 250); rect(625, 125, 250, 250); rect(625, 375, 250, 250); rect(625, 625, 250, 250);
+  rect(125, 125, 250, 250); rect(125, 375, 250, 250); rect(125, 625, 250, 250); rect(375, 125, 250, 250); rect(375, 375, 250, 250); rect(375, 625, 250, 250); rect(625, 125, 250, 250); rect(625, 375, 250, 250); rect(625, 625, 250, 250); //main board rectangles
   if (box1 == 1) {
     cross(posX = 175, posY = 175) //box1 pos 
   }
@@ -90,7 +92,7 @@ function draw() {
   strokeWeight(20)
   // wins player 1
   if (box1 == 1 && box2 == 1 && box3 == 1) {
-    player = 3
+    player = 3 //player 3 means player 1 has won
     line(180, 250, 820, 250)
   }
   if (box4 == 1 && box5 == 1 && box6 == 1) {
@@ -123,7 +125,7 @@ function draw() {
   }
   // wins player 2
   if (box1 == 2 && box2 == 2 && box3 == 2) {
-    player = 4
+    player = 4 //player 4 means player 2 has won
     line(180, 250, 820, 250)
   }
   if (box4 == 2 && box5 == 2 && box6 == 2) {
@@ -155,16 +157,16 @@ function draw() {
     line(170, 830, 825, 175)
   }
   if (turns == 9 && player <= 2) {
-    player = 5
+    player = 5 // player 5 means a tie
   }
 }
 function mousePressed() {
   if (mouseX >= 150 && mouseX < 375 && mouseY > 125 && mouseY < 375) { //box 1 detection
     if (box1 == 0) {
       if (player == 1) {
-        box1 = 1
-        player = 2
-        turns += 1
+        box1 = 1 //this is used to identify if something is placed
+        player = 2 //this changes it to the second player
+        turns += 1 //this adds a turn used for ties 
       }
       else if (player == 2) {
         box1 = 2
@@ -286,11 +288,11 @@ function mousePressed() {
     }
   }
 }
-function cross() {
+function cross() { //function made to make a cross
   line(0 + posX, 0 + posY, 150 + posX, 150 + posY)
   line(0 + posX, 150 + posY, 150 + posX, 0 + posY)
 }
-function circle2() {
+function circle2() { //function made to make a circle
   noFill()
   circle(0 + posX, 0 + posY, 200)
   fill('black')
@@ -307,11 +309,14 @@ function keyPressed() {
     box8 = 0
     box9 = 0
     turns = 0
+    gamecount += 1
     if (player == 3) {
       player = 2
+      player1wins += 1
     }
     if (player == 4) {
       player = 1
+      player2wins += 1
     }
   }
 }
