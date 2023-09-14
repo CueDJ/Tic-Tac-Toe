23552441b0187d4e15dad4ddbd5afeb47f279a89
Jesper Heese <JesperHeese@outlook.com>
2023-09-14 10:42:04 +0200

    removed debugging

--- sketch.js
+++ sketch.js
@@ -9,27 +9,7 @@ strokeWeight(5)
 
 function draw() {
   background(220);
-  text(('mouse Y:' + mouseY), 20, 40) //remove later
-  text(('mouse x:' + mouseX), 20, 20) //remove later
   rect(125,125,250,250); rect(125,375,250,250); rect(125,625,250,250); rect(375,125,250,250); rect(375,375,250,250); rect(375,625,250,250); rect(625,125,250,250); rect(625,375,250,250); rect(625,625,250,250);
-/*  cross(posX = 175, posY = 175) //box1 pos 
-  cross(posX = 425, posY = 175) //box2 pos
-  cross(posX = 675, posY = 175) //box3 pos 
-  cross(posX = 175, posY = 425) //box4 pos
-  cross(posX = 425, posY = 425) //box5 pos 
-  cross(posX = 675, posY = 425) //box6 pos
-  cross(posX = 175, posY = 675) //box7 pos 
-  cross(posX = 425, posY = 675) //box8 pos
-  cross(posX = 675, posY = 675) //box9 pos
-  circle2(posX = 250, posY = 250) //box1 pos
-  circle2(posX = 500, posY = 250) //box2 pos
-  circle2(posX = 750, posY = 250) //box3 pos
-  circle2(posX = 250, posY = 500) //box4 pos
-  circle2(posX = 500, posY = 500) //box5 pos
-  circle2(posX = 750, posY = 500) //box6 pos
-  circle2(posX = 250, posY = 750) //box7 pos
-  circle2(posX = 500, posY = 750) //box8 pos
-  circle2(posX = 750, posY = 750) //box9 pos */
   if(box1 == 1) {
     cross(posX = 175, posY = 175) //box1 pos 
   }
@@ -85,7 +65,6 @@ function draw() {
     circle2(posX = 750, posY = 750) //box9 pos
   }
 
-
   // wins player 1
   if(box1 == 1 && box2 == 1 && box3 == 1) {
     console.log('player 1 wins!')
@@ -114,6 +93,7 @@ function draw() {
   if(box7 == 1 && box5 == 1 && box3 == 1) {
     console.log('player 1 wins!')
   }
+
   // wins player 2
   if(box1 == 2 && box2 == 2 && box3 == 2) {
     console.log('player 2 wins!')
@@ -144,22 +124,16 @@ function draw() {
   }
 }
 function mousePressed() {
-  
   if(mouseX >= 150 && mouseX < 375 && mouseY >125 && mouseY < 375) { //box 1 detection
     console.log('box 1')
     if(box1 == 0) {
-      console.log('test')
       if(player == 1) {
         box1 = 1
         player = 2
-        console.log(box1)
       }
       else if(player == 2) {
         box1 = 2
         player = 1
-      }
-      else {
-      console.log('code brok')
       }
     }
   }
@@ -174,9 +148,6 @@ function mousePressed() {
         box2 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 625 && mouseX < 875 && mouseY >=125 && mouseY < 375) { //box 3 detection
@@ -190,13 +161,9 @@ function mousePressed() {
         box3 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 150 && mouseX < 375 && mouseY >375 && mouseY < 625) { //box 4 detection
-    console.log('box 4')
     if(box4 == 0) {
       if(player == 1) {
         box4 = 1
@@ -206,13 +173,9 @@ function mousePressed() {
         box4 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 375 && mouseX < 625 && mouseY >375 && mouseY < 625) { //box 5 detection
-    console.log('box 5')
     if(box5 == 0) {
       if(player == 1) {
         box5 = 1
@@ -222,13 +185,9 @@ function mousePressed() {
         box5 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 625 && mouseX < 875 && mouseY >375 && mouseY < 625) { //box 6 detection
-    console.log('box 6')
     if(box6 == 0) {
       if(player == 1) {
         box6 = 1
@@ -238,13 +197,9 @@ function mousePressed() {
         box6 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 150 && mouseX < 375 && mouseY >625 && mouseY < 875) { //box 7 detection
-    console.log('box 7')
     if(box7 == 0) {
       if(player == 1) {
         box7 = 1
@@ -254,13 +209,9 @@ function mousePressed() {
         box7 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 375 && mouseX < 625 && mouseY >625 && mouseY < 875) { //box 8 detection
-    console.log('box 8')
     if(box8 == 0) {
       if(player == 1) {
         box8 = 1
@@ -270,13 +221,9 @@ function mousePressed() {
         box8 = 2
         player = 1
       }
-      else {
-      console.log('code brok')
-      }
     }
   }
   if(mouseX >= 625 && mouseX < 875 && mouseY >625 && mouseY < 875) { //box 9 detection
-    console.log('box 9')
     if(box9 == 0) {
       if(player == 1) {
         box9 = 1
@@ -286,9 +233,6 @@ function mousePressed() {
         box9 = 2
         player = 1
 
-      }
-      else {
-      console.log('code brok')
       }
     }
   } 
